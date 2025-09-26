871ef225a490174b6cf6de76e64b177123d0d6ba
serpentaa <l.salav@yandex.ru>
2025-09-26 10:47:24 +0500

    Правка кода

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function add() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    
-    if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById("result").innerText = "Введите числа!";
-    } else {
-        var result = num1 + num2;
-        document.getElementById("result").innerText = result;
+const inputOne = document.getElementById('input1')
+const inputTwo = document.querySelector('#input2')
+const btn = document.getElementById('btn')
+const output = document.getElementById('output')
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const numberOrNaN1 = Number(inputOne.value)
+    const numberOrNaN2 = Number(inputTwo.value)
+let result = ''
+    if(isNaN(numberOrNaN1) && isNaN(numberOrNaN2)) {
+        result = 'Введите числа!'
     }
-}
+    if(isNaN(numberOrNaN1) && !isNaN(numberOrNaN2)) {
+        result = 'Введите число в первое поле!'
+    }
+    if(!isNaN(numberOrNaN1) && isNaN(numberOrNaN2)) {
+        result = 'Введите число во второе поле!'
+    }
+    if(!isNaN(numberOrNaN1) && !isNaN(numberOrNaN2)) {
+        result = `Результат = ${numberOrNaN1 + numberOrNaN2}`
+    }
+    output.innerText = result
+})
